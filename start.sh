334a6e6884cf388cc9f5dd893ae99d032c7da0cb
trypokaridos <trypokaridos@lolek.o25th.dyndns.org>
2016-01-27 12:42:54 +0200

    Add Script to Fix User permissions

--- start.sh
+++ start.sh
@@ -15,6 +15,24 @@ fi
 
 www=${DRUPAL_DOCROOT}
 
+#!/bin/sh
+
+export ORIGPASSWD=$(cat /etc/passwd | grep dockerdev)
+export ORIG_UID=$(echo $ORIGPASSWD | cut -f3 -d:)
+export ORIG_GID=$(echo $ORIGPASSWD | cut -f4 -d:)
+
+export DEV_UID=${DEV_UID:=$ORIG_UID}
+export DEV_GID=${DEV_GID:=$ORIG_GID}
+
+ORIG_HOME=$(echo $ORIGPASSWD | cut -f6 -d:)
+
+sed -i -e "s/:$ORIG_UID:$ORIG_GID:/:$DEV_UID:$DEV_GID:/" /etc/passwd
+sed -i -e "s/dockerdev:x:$ORIG_GID:/dockerdev:x:$DEV_GID:/" /etc/group
+
+chown -R ${DEV_UID}:${DEV_GID} ${DRUPAL_DOCROOT}
+
+exec su - dockerdev
+
 # allow build tracking outside this script/container
 # only updated during building, not running.
 buildstat="/var/www/html/.start.sh.stat";   
